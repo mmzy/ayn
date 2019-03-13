fef9336a5de61db8b64a35d8061c61f152d2dc1d
zhengyunyi <0049002911@znv.com>
2019-03-13 15:14:10 +0800

    sleep

--- src/layouts/Sleep.vue
+++ src/layouts/Sleep.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="container">
-    <h2>sleep {{sec}}s:</h2>
+    <h2>sleep {{sec}}s, 计时:{{timerSec}}s</h2>
     <div class="demo1">
       <p>方法一：promise</p>
       <input type="button" value="sleep1" class="btn" @click="sleep1"/>
     </div>
     <div class="demo2">
-      <p>方法二：promise, asyn, await</p>
+      <p>方法二：promise, async, await</p>
       <button class="btn" @click="sleep2">sleep2</button>
     </div>
     <div class="demo3">
       <p>方法三：generate</p>
       <button class="btn" @click="sleep3">sleep3</button>
+    </div>
+    <div class="demo4">
+      <p>方法四：伪死循环</p>
+      <input type="button" value="sleep4" class="btn" @click="sleep4"/>
     </div>
   </div>
 </template>
@@ -20,7 +24,8 @@
 export default {
   data () {
     return {
-      sec: 10
+      sec: 5,
+      timerSec: 0
     }
   },
   methods: {
@@ -32,6 +37,19 @@ export default {
     },
     sleep3: function () {
       alert(3)
+    },
+    sleep4 () {
+      let startTime = new Date().getTime()
+      let thisObj = this
+      thisObj.timerSec = 0
+      let timer = setInterval(() => {
+        thisObj.timerSec++
+      }, 1000)
+      while (new Date().getTime() - startTime < thisObj.sec * 1000) {
+        continue
+      }
+      alert(4)
+      clearInterval(timer)
     }
   }
 }
@@ -46,7 +64,7 @@ export default {
   text-align: left;
   border: solid 1px black;
   div {
-    margin: 50px;
+    margin: 20px;
     .btn {
       width: 200px;
       height: 50px;
